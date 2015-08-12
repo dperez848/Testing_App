d9d6da74226e3bf0f9fcfe8defc0db19b8e5ad87
dperez1992 <danyperez92@gmail.com>
2015-08-12 16:12:42 -0430

    Second

--- app/src/main/java/com/example/uppersky_movil/myapplication/MainActivity.java
+++ app/src/main/java/com/example/uppersky_movil/myapplication/MainActivity.java
@@ -12,6 +12,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
@@ -51,6 +54,21 @@ public class MainActivity extends BaseActivity {
         setToolbar(mToolbar, "Main", 3);
 
         navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
+
+
+        final View actionB = findViewById(R.id.action_b);
+
+        FloatingActionButton actionC = new FloatingActionButton(getBaseContext());
+        actionC.setTitle("Hide/Show Action above");
+        actionC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actionB.setVisibility(actionB.getVisibility() == View.GONE ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        final FloatingActionsMenu menuMultipleActions = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
+        menuMultipleActions.addButton(actionC);
 
 
     }
